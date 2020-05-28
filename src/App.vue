0c7665e3bba8249ecead3c27bd067abaee87c730
Liangxiyao <790680635@qq.com>
2020-05-28 13:52:18 +0800

    5.28

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template>
   <div id="app">
-    <m-header></m-header>
-    <tab></tab>
+    
     <keep-alive><router-view/></keep-alive>
   </div>
 </template>
 
 <script>
-import MHeader from 'component/m-header/m-header'
-import Tab from 'component/tab/tab'
-
 export default {
   name: 'App',
   components:{
-    MHeader,
-    Tab
+    
   }
 }
 </script>
